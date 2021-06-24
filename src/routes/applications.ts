9f75b2deb86a003dd16891d9743c8d8b21e8ac5e
blabadi <bashar.allabadi@oicr.on.ca>
2021-06-24 15:38:37 -0400

    zip package endpoint (#51)
    rename signedAt to uploadedAt
    fix health endpoint

--- src/routes/applications.ts
+++ src/routes/applications.ts
@@ -2,8 +2,9 @@ import { Router, Request, Response, RequestHandler } from 'express';
 
 
 import wrapAsync from '../utils/wrapAsync';
-import { create, createCollaborator, deleteApp, deleteCollaborator, getById, search, updateCollaborator, uploadDocument, updatePartial, deleteDocument } from '../domain/service';
+import { create, createCollaborator, deleteApp, deleteCollaborator, getById, search, updateCollaborator, uploadDocument, updatePartial, deleteDocument, getApplicationAssetsAsStream } from '../domain/service';
 import { BadRequest } from '../utils/errors';
+import logger from '../logger';
 import { Identity } from '@overture-stack/ego-token-middleware';
 import { Application } from '../domain/interface';
 import { AppConfig } from '../config';
@@ -11,6 +12,10 @@ import _ from 'lodash';
 import { Storage } from '../storage';
 import { Transporter } from 'nodemailer';
 import SMTPTransport from 'nodemailer/lib/smtp-transport';
+// https://www.archiverjs.com/docs/quickstart
+import archiver from 'archiver';
+import moment from 'moment';
+import { Readable } from 'stream';
 interface IRequest extends Request {
   identity: Identity;
 }
@@ -28,8 +33,40 @@ const createApplicationsRouter = (config: AppConfig,
       const appId = validateId(req.params.id);
       const type = validateType(req.params.type) as 'ETHICS' | 'SIGNED_APP';
       const objectId = req.params.assetId;
+      logger.info(`delete document [app: ${appId}, type: ${type}, assetId: ${objectId}, user Id:${(req as IRequest).identity.userId}]`);
       const app = await deleteDocument(appId, type, objectId, (req as IRequest).identity, storageClient);
       return res.status(200).send(app);
+    })
+  );
+
+  router.get('/applications/:id/assets/APP_PACKAGE',
+    authFilter([]),
+    wrapAsync(async (req: Request, res: Response) => {
+      const appId = validateId(req.params.id);
+      logger.info(`download app package [app: ${appId}, user Id:${(req as IRequest).identity.userId}]`);
+      const assets = await getApplicationAssetsAsStream(appId, (req as IRequest).identity, storageClient);
+      const zip = archiver('zip', {
+        zlib: { level: 0 }
+      });
+
+      // append all asset streams to the zip archive
+      assets.forEach(a => {
+        zip.append(a.stream as Readable, {name:  a.name});
+      });
+
+      const zipName = `${appId}_${moment().format('YYYYMMDDTHHmm')}.zip`;
+      res.attachment(zipName);
+
+      // pipe the zip stream output to the response directly
+      zip.pipe(res);
+
+      // on zip finish end the response
+      zip.on('finish', () => {
+        res.status(200).end();
+      });
+
+      // finialize the zip.
+      zip.finalize();
     })
   );
 
@@ -45,6 +82,7 @@ const createApplicationsRouter = (config: AppConfig,
       }
       const appId = validateId(req.params.id);
       const type = validateType(req.params.type) as 'ETHICS' | 'SIGNED_APP';
+      logger.info(`upload app file [app: ${appId}, type: ${type}, file: ${uploadedFile.name}, user Id:${(req as IRequest).identity.userId}]`);
       const app = await uploadDocument(appId, type, uploadedFile, (req as IRequest).identity, storageClient);
       return res.status(201).send(app);
    })
@@ -54,6 +92,7 @@ const createApplicationsRouter = (config: AppConfig,
     '/applications/',
     authFilter([]),
     wrapAsync(async (req: Request, res: Response) => {
+      logger.info(`creating new application [user id: ${(req as IRequest).identity.userId}]`);
       const app = await create((req as IRequest).identity);
       return res.status(201).send(app);
     }),
@@ -67,6 +106,7 @@ const createApplicationsRouter = (config: AppConfig,
       const validatedId = validateId(id);
       // todo validate structure
       const collaborator = req.body;
+      logger.info(`creating new collaborator [app: ${id}, user Id:${(req as IRequest).identity.userId}]`);
       const app = await createCollaborator(validatedId, collaborator, (req as IRequest).identity);
       return res.status(200).send(app);
     }),
@@ -81,6 +121,7 @@ const createApplicationsRouter = (config: AppConfig,
       const collaboratorId = req.params.collaboratorId;
       // todo validate structure
       const collaborator = req.body;
+      logger.info(`updating collaborators [app: ${id}, collaboratorId: ${collaboratorId}, user Id:${(req as IRequest).identity.userId}]`);
       const app = await updateCollaborator(validatedId, collaborator, (req as IRequest).identity);
       return res.status(200).send(app);
     }),
@@ -93,6 +134,7 @@ const createApplicationsRouter = (config: AppConfig,
       const id = req.params.id;
       const validatedId = validateId(id);
       const collaboratorId = req.params.collaboratorId;
+      logger.info(`deleting collaborator [app: ${id}, collaboratorId: ${collaboratorId}, user Id:${(req as IRequest).identity.userId}]`);
       const app = await deleteCollaborator(validatedId, collaboratorId, (req as IRequest).identity);
       return res.status(200).send(app);
     }),
@@ -120,7 +162,7 @@ const createApplicationsRouter = (config: AppConfig,
         sort,
         sortBy
       };
-
+      logger.info(`searching applications [query: ${JSON.stringify(params)}, user Id:${(req as IRequest).identity.userId}]`);
       const app = await search(params, (req as IRequest).identity);
       return res.status(200).send(app);
     }),
@@ -132,6 +174,7 @@ const createApplicationsRouter = (config: AppConfig,
     wrapAsync(async (req: Request, res: Response) => {
       const id = req.params.id;
       const validatedId = validateId(id);
+      logger.info(`fetching application [app: ${id}, user Id:${(req as IRequest).identity.userId}]`);
       const result = await getById(validatedId, (req as IRequest).identity);
       if (!result) {
         return res.status(404).send();
@@ -146,6 +189,7 @@ const createApplicationsRouter = (config: AppConfig,
     wrapAsync(async (req: Request, res: Response) => {
       const id = req.params.id;
       const validatedId = validateId(id);
+      logger.info(`deleting application [app: ${id}, user Id:${(req as IRequest).identity.userId}]`);
       await deleteApp(validatedId, (req as IRequest).identity);
       return res.status(200).end();
     }),
@@ -159,6 +203,7 @@ const createApplicationsRouter = (config: AppConfig,
       const validatedId = validateId(id);
       const app = req.body as Application;
       app.appId = id;
+      logger.info(`updating application [app: ${id}, user Id:${(req as IRequest).identity.userId}]`);
       const updated = await updatePartial(app, (req as IRequest).identity, storageClient, emailClient);
       return res.status(200).send(updated);
     }),
@@ -179,10 +224,10 @@ function validateId(id: string) {
 }
 
 function validateType(type: string) {
-  if (!['ETHICS', 'SIGNED_APP'].includes(type)) {
+  if (!['ETHICS', 'SIGNED_APP', 'ethics', 'signed_app'].includes(type)) {
     throw new BadRequest('unknow document type, should be ETHICS or SIGNED_APP');
   }
-  return type;
+  return type.toUpperCase();
 }
 
 export default createApplicationsRouter;
