ba0053e6ae645f5d19e76280af02c10528e227ba
Ann Catton <anncatton@gmail.com>
2021-08-04 12:52:00 -0400

    Update expected appendix agreements

--- src/domain/interface.ts
+++ src/domain/interface.ts
@@ -1,6 +1,24 @@
-export type State =  'DRAFT' | 'SIGN AND SUBMIT' | 'REVIEW' | 'REVISIONS REQUESTED' | 'APPROVED' | 'RENEWING' | 'REJECTED' | 'CLOSED' | 'EXPIRED' ;
+export type State =
+  | 'DRAFT'
+  | 'SIGN AND SUBMIT'
+  | 'REVIEW'
+  | 'REVISIONS REQUESTED'
+  | 'APPROVED'
+  | 'RENEWING'
+  | 'REJECTED'
+  | 'CLOSED'
+  | 'EXPIRED';
 
-export type SectionStatus = 'PRISTINE' | 'COMPLETE' | 'INCOMPLETE' | 'REVISIONS REQUESTED' | 'LOCKED' | 'DISABLED' | 'REVISIONS REQUESTED DISABLED' | 'REVISIONS MADE' | 'AMMENDABLE';
+export type SectionStatus =
+  | 'PRISTINE'
+  | 'COMPLETE'
+  | 'INCOMPLETE'
+  | 'REVISIONS REQUESTED'
+  | 'LOCKED'
+  | 'DISABLED'
+  | 'REVISIONS REQUESTED DISABLED'
+  | 'REVISIONS MADE'
+  | 'AMMENDABLE';
 
 export type UploadDocumentType = 'ETHICS' | 'SIGNED_APP';
 interface Meta {
@@ -55,9 +73,9 @@ interface ApplicationUpdate {
 
 export interface SearchResult {
   pagingInfo: {
-    totalCount: number,
-    pagesCount: number,
-    index: number,
+    totalCount: number;
+    pagesCount: number;
+    index: number;
   };
   items: ApplicationSummary[];
 }
@@ -77,18 +95,17 @@ export interface ApplicationSummary {
     declaredAsRequired: boolean | undefined;
   };
   applicant: {
-    info: PersonalInfo,
+    info: PersonalInfo;
   };
   collaborators?: PersonalInfo[];
 }
 
-
 export type ApplicationDto = Omit<Application, 'searchField'>;
 
 export type SectionError = {
-  field: string,
-  message: string,
-  code?: string
+  field: string;
+  message: string;
+  code?: string;
 };
 export interface Application {
   appId: string;
@@ -106,73 +123,83 @@ export interface Application {
   createdAtUtc?: Date;
   searchValues: string[];
   revisionRequest: {
-    applicant: RevisionRequest,
-    representative: RevisionRequest,
-    projectInfo: RevisionRequest,
-    collaborators: RevisionRequest,
-    signature: RevisionRequest,
-    ethicsLetter: RevisionRequest,
-    general: RevisionRequest
+    applicant: RevisionRequest;
+    representative: RevisionRequest;
+    projectInfo: RevisionRequest;
+    collaborators: RevisionRequest;
+    signature: RevisionRequest;
+    ethicsLetter: RevisionRequest;
+    general: RevisionRequest;
   };
   sections: {
     terms: {
-      meta: Meta,
-      agreement: AgreementItem
-    },
+      meta: Meta;
+      agreement: AgreementItem;
+    };
     applicant: {
-      meta: Meta,
-      info: PersonalInfo,
-      address: Address
-    },
+      meta: Meta;
+      info: PersonalInfo;
+      address: Address;
+    };
     representative: {
-      meta: Meta,
-      info: PersonalInfo,
+      meta: Meta;
+      info: PersonalInfo;
       addressSameAsApplicant: boolean;
-      address: Address | undefined
-    },
+      address: Address | undefined;
+    };
     collaborators: {
-      meta: Meta,
-      list: Collaborator[],
-    },
+      meta: Meta;
+      list: Collaborator[];
+    };
     projectInfo: {
-      meta: Meta,
+      meta: Meta;
       title: string;
       website: string;
       background: string;
       aims: string;
       summary: string;
       methodology: string;
-      publicationsURLs: string[]
-    },
+      publicationsURLs: string[];
+    };
     ethicsLetter: {
-      meta: Meta,
+      meta: Meta;
       declaredAsRequired: boolean | null;
       approvalLetterDocs: {
         objectId: string;
         uploadedAtUtc: Date;
         name: string;
       }[];
-    },
+    };
     dataAccessAgreement: {
-      meta: Meta,
-      agreements: AgreementItem[],
-    },
+      meta: Meta;
+      agreements: AgreementItem[];
+    };
     appendices: {
-      meta: Meta,
-      agreements: AgreementItem[],
-    },
+      meta: Meta;
+      agreements: AgreementItem[];
+    };
     signature: {
-      meta: Meta,
+      meta: Meta;
       signedAppDocObjId: string;
       uploadedAtUtc?: Date;
       signedDocName: string;
-    }
+    };
   };
   updates: ApplicationUpdate[];
 }
 
 export type AppSections = keyof Application['sections'];
-export type RevisionSections = keyof Pick<Record<AppSections, RevisionRequest>, 'signature' | 'projectInfo' | 'applicant' | 'representative' | 'collaborators' | 'ethicsLetter'> | 'general';
+export type RevisionSections =
+  | keyof Pick<
+      Record<AppSections, RevisionRequest>,
+      | 'signature'
+      | 'projectInfo'
+      | 'applicant'
+      | 'representative'
+      | 'collaborators'
+      | 'ethicsLetter'
+    >
+  | 'general';
 export type RevisionRequestUpdate = Partial<Record<RevisionSections, RevisionRequest>>;
 export interface UpdateApplication {
   state?: State;
@@ -181,20 +208,20 @@ export interface UpdateApplication {
   revisionRequest?: RevisionRequestUpdate;
   sections: {
     terms?: {
-      agreement: AgreementItem
-    },
+      agreement: AgreementItem;
+    };
     applicant?: {
-      info?: Partial<PersonalInfo>,
-      address?: Partial<Address>
-    },
+      info?: Partial<PersonalInfo>;
+      address?: Partial<Address>;
+    };
     representative?: {
-      info?: Partial<PersonalInfo>,
+      info?: Partial<PersonalInfo>;
       addressSameAsApplicant?: boolean;
-      address?: Partial<Address>
-    },
+      address?: Partial<Address>;
+    };
     collaborators?: {
-      list: Collaborator[],
-    },
+      list: Collaborator[];
+    };
     projectInfo?: {
       title?: string;
       website?: string;
@@ -202,30 +229,30 @@ export interface UpdateApplication {
       aims?: string;
       methodology?: string;
       summary?: string;
-      publicationsURLs?: string[]
-    },
+      publicationsURLs?: string[];
+    };
     ethicsLetter?: {
       declaredAsRequired?: boolean | null;
       approvalLetterDocs?: {
         name: string;
         objectId: string;
-        uploadedAtUtc: Date
+        uploadedAtUtc: Date;
       }[];
-    },
+    };
     dataAccessAgreement?: {
-      agreements: AgreementItem[],
-    },
+      agreements: AgreementItem[];
+    };
     appendices?: {
-      agreements: AgreementItem[],
-    },
+      agreements: AgreementItem[];
+    };
     signature?: {
       signedAppDocObjId: string;
-    }
+    };
   };
 }
 
 export enum FileFormat {
-  DACO_FILE_FORMAT = 'daco-file-format'
+  DACO_FILE_FORMAT = 'daco-file-format',
 }
 
 export type CSVFileHeader = {
@@ -239,20 +266,15 @@ export const IT_AGREEMENT_PROTECT_DATA = 'it_agreement_protect_data';
 export const IT_AGREEMENT_MONITOR_ACCESS = 'it_agreement_monitor_access';
 export const IT_AGREEMENT_DESTROY_COPIES = 'it_agreement_destroy_copies';
 export const IT_AGREEMENT_ONBOARD_TRAINING = 'it_agreement_onboard_training';
-export const IT_AGREEMENT_PROVIDE_INSTITUTIONAL_POLICIES = 'it_agreement_provide_institutional_policies';
+export const IT_AGREEMENT_PROVIDE_INSTITUTIONAL_POLICIES =
+  'it_agreement_provide_institutional_policies';
 export const IT_AGREEMENT_CONTACT_DACO_FRAUD = 'it_agreement_contact_daco_fraud';
 export const IT_AGREEMENT_CLOUD_USAGE_RISK = 'it_agreement_cloud_usage_risk';
 export const IT_AGREEMENT_READ_CLOUD_APPENDIX = 'it_agreement_read_cloud_appendix';
 
-
-export const  APPENDIX_ICGC_GOALS_POLICIES = 'appendix_icgc_goals_policies';
-export const  APPENDIX_LARGE_SCALE_DATA_SHARING = 'appendix_large_scale_data_sharing';
-export const  APPENDIX_PREPUBLICATION_POLICY = 'appendix_prepublication_policy';
-export const  APPENDIX_PUBLICATION_POLICY = 'appendix_publication_policy';
-export const  APPENDIX_NIH_GENOMIC_INVENTIONS = 'appendix_nih_genomic_inventions';
-export const  APPENDIX_OECD_GENETIC_INVENTIONS = 'appendix_oecd_genetic_inventions';
-export const  APPENDIX_CLOUD_SECURITY = 'appendix_cloud_security';
-export const  APPENDIX_GA4GH_FRAMEWORK = 'appendix_ga4gh_framework';
+export const APPENDIX_ICGC_GOALS_POLICIES = 'appendix_icgc_goals_policies';
+export const APPENDIX_DATA_ACCESS_POLICY = 'appendix_data_access_policy';
+export const APPENDIX_IP_POLICY = 'appendix_ip_policy';
 
 export const DAA_CORRECT_APPLICATION_CONTENT = 'daa_correct_application_content';
 export const DAA_AGREE_TO_TERMS = 'daa_agree_to_terms';
