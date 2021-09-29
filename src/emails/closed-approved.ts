b77d914ff1fa74b4f3ac9e1faf9c61111535586d
Ann Catton <anncatton@gmail.com>
2021-09-29 10:07:41 -0400

    💬 Update closed-approved email content

--- src/emails/closed-approved.ts
+++ src/emails/closed-approved.ts
@@ -1,16 +1,17 @@
-import {
-  ICGC_ARGO_CONTACT_URL,
-} from '../utils/constants';
+import { ICGC_ARGO_CONTACT_URL } from '../utils/constants';
 import { AppConfig } from '../config';
 import { Application, Collaborator, PersonalInfo } from '../domain/interface';
-import { appInfoBox, approvalDetailsBox, approvalDetailsContent, compose, formatDate, textParagraphSection } from './common';
+import {
+  appInfoBox,
+  approvalDetailsBox,
+  approvalDetailsContent,
+  compose,
+  formatDate,
+  textParagraphSection,
+} from './common';
 import { compileMjmlInPromise } from './mjml';
 
-export default async function (
-  app: Application,
-  linksConfigs: AppConfig['email']['links'],
-
-) {
+export default async function (app: Application, linksConfigs: AppConfig['email']['links']) {
   const applicantInfo = app.sections.applicant.info;
   const emailMjml = compose(
     {
@@ -61,7 +62,7 @@ function messageBody(app: Application, recipient: PersonalInfo, surveyUrl: strin
       { padding: '0px 0px 20px 0px' },
     )}
     ${textParagraphSection(
-      `We would appreciate any feedback on your successes and challenges with accessing ICGC Controlled Data and the outcomes of your research project. Please take a moment to <a href="${surveyUrl}">fill out this short feedback survey</a>.`,
+      `You are required to complete a final report as per the conditions of the Data Access Agreement. <a href="${surveyUrl}">Click here to fill out the report</a>, describing your successes and challenges with accessing ICGC Controlled Data and the outcomes of your research project.`,
       { padding: '0px 0px 20px 0px' },
     )}
     ${textParagraphSection(
