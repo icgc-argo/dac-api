fe52f9ef653c1d19d1b9cfd61aac3ce6f6f0b90b
Ann Catton <anncatton@gmail.com>
2023-02-01 17:55:25 -0500

    implement close renewals job, correct query

--- src/jobs/runAllJobs.ts
+++ src/jobs/runAllJobs.ts
@@ -10,7 +10,8 @@ import runExpiringAppsCheck from './expireAppCheck';
 import approvedUsersEmail from './approvedUsersEmail';
 import firstExpiryNotificationCheck from './firstExpiryNotification';
 import secondExpiryNotificationCheck from './secondExpiryNotification';
-import { Report, JobReport } from './types';
+import runCloseUnsubmittedRenewalsCheck from './closeUnsubmittedRenewalsCheck';
+import { Report } from './types';
 
 const JOB_NAME = 'ALL BATCH JOBS';
 
@@ -37,28 +38,21 @@ export default async function (
       emailClient,
     );
     const expiringAppsReport = await runExpiringAppsCheck(currentDate, emailClient, user);
-    // TODO: const closedAppsReport
+    const closedRenewalsReport = await runCloseUnsubmittedRenewalsCheck(currentDate, user);
     const approvedUsersEmailReport = await approvedUsersEmail(emailClient);
     // define report to collect all affected appIds
     // each job will return its own report
     // this function will build a complete summary
     // will simply log the summary for now until we decide what to do with it
     logger.info(`${JOB_NAME} - Completed, generating report.`);
-    // TODO: remove once expiry reports are implemented, just making ts happy
-    const getReportToBeImplemented: (jobName: string) => JobReport<any> = (jobName) => ({
-      jobName,
-      startedAt: new Date(),
-      finishedAt: new Date(),
-      success: false,
-    });
+
     const completeReport: Report = {
       attestationNotifications: attestationNotificationReport,
       pausedApps: pausedAppReport,
-      // TODO: implement expiry/renewal jobs. Add to report
       expiryNotifications1: firstExpiryNotificationReport,
       expiryNotifications2: secondExpiryNotificationReport,
       expiredApps: expiringAppsReport,
-      closedApps: getReportToBeImplemented('CLOSING EXPIRED APPLICATIONS'),
+      closedApps: closedRenewalsReport,
       approvedUsers: approvedUsersEmailReport,
     };
     logger.info(`${JOB_NAME} - Logging report`);
